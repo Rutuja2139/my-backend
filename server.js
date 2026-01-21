6109625617e2854523ef05b13022c62d87286c62
Rutuja2139 <wadgaonkarrutuja1@gmail.com>
2026-01-21 14:55:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,15 +13,7 @@ connectDB();
 
  app.use('/', AllRoutes);
 
-// app.post('/login',(req,res)=>{
-//     console.log(req.body);
-//     res.status(200).json({success:true})
-// })
 
-// app.post('/', upload.single('excelFile') ,(req,res)=>{
-//     console.log(req.file);
-//     res.status(200).json({'success': true, 'data': 'connected to backend'})
-// })
 
 app.listen(process.env.PORT, ()=>{
     console.log("server connected")
