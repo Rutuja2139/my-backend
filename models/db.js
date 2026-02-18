b6076f1ea0e0ab2e9cd944f412989f67d0f7bec5
Rutuja2139 <wadgaonkarrutuja1@gmail.com>
2026-02-18 17:28:49 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -8,7 +8,7 @@ const connectDB = async() =>{
         {
           useNewUrlParser: true,
         });
-        console.log(`MongoDB Connected: {conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
       } catch (error) {
         console.error(error.message);
         process.exit(1);
